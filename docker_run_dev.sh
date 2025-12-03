79ce06b38f7075a5bbff2090ae2322bdd4746efa
Lidia <lidix91@users.noreply.github.com>
2025-12-03 19:22:41 +0100

    Update docker_run_dev.sh

--- docker_run_dev.sh
+++ docker_run_dev.sh
@@ -1,1 +1,1 @@
-docker run -d -p 4200:80 --rm --network mls-network --name mls_toolbox_client mls_toolbox_client
+docker run -d -p 4201:80 --rm --network mls-network --name mls_toolbox_client mls_toolbox_client
