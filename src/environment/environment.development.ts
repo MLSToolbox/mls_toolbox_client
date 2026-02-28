b46df957adf26db8afb6401d0567e90fb24dbf9a
beto <betooxx.dev@gmail.com>
2026-02-28 14:18:39 -0600

    fix: api url

--- src/environment/environment.development.ts
+++ src/environment/environment.development.ts
@@ -4,7 +4,7 @@ import { validateEnvironment } from './environment.schema';
 // Nginx proxy: puerto 1448 → backend development (5001)
 
 const config = {
-  apiUrl: 'http://gessi.cs.upc.edu:1448/api',
+  apiUrl: 'https://gessi.cs.upc.edu:1448/api',
   apiTimeout: 30000,
 };
 
